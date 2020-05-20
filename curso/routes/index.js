ab13e36e3b2542aab02d1b140e7a1a753d44114b
gianlucagave98 <gianluca.gave@tecsup.edu.pe>
2020-05-19 19:13:09 -0500

    Primer commit by Gave

--- curso/routes/index.js
+++ curso/routes/index.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
 
+let transaccion = {
+  curso: '',
+  modulos: [],
+  pago: '',
+  monto: 0
+}
+
+const calculo = (pago) => { 
+  pago = pago - (0.1 * pago)
+  return pago
+}
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router
+  .get('/', function (req, res, next) { res.render('index') })
+  .post('/curso', (req, res) => {
+    let curso = req.body.cursos
+    transaccion.modulos = []
+    if (curso == 'Java') {
+      transaccion.monto = 1200
+    } else if (curso == 'PHP') {
+      transaccion.monto = 800
+    } else { 
+      transaccion.monto = 1500
+    }
+    transaccion.curso = curso
+    res.redirect('/modulos')
+   })
+
+router
+  .get('/modulos', (req, res) => { res.render('modulos')})
+  .post('/modulos', (req, res, next) => { 
+    let module = req.body.modulos
+    let module1 = req.body.modulos1
+    let module2 = req.body.modulos2
+    if(module) transaccion.modulos.push(module)
+    if(module1) transaccion.modulos.push(module1)
+    if (module2) transaccion.modulos.push(module2)
+    res.redirect('/pagar')
+  })
 
 module.exports = router;
